49f23b56ec62ea389f4f3b2d734815f479766c48
Nicolas Bourdin <nicolas.bourdin@infomaniak.com>
2024-11-14 23:11:31 +0100

    feat: Implement custom text and animated text

--- androidApp/src/main/java/com/ames/fr/android/ui/EventsManager.kt
+++ androidApp/src/main/java/com/ames/fr/android/ui/EventsManager.kt
@@ -1,14 +1,19 @@
 package com.ames.fr.android.ui
 
 import android.content.res.Resources
-import androidx.compose.material3.Text
+import android.util.Log
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
 import com.ames.fr.android.R
 import com.ames.fr.android.ui.element.CustomButton
+import com.ames.fr.android.ui.element.CustomText
 import com.ames.fr.data.ScriptManager
 import com.ames.fr.data.model.Event
 import com.ames.fr.data.model.TypeEvent
@@ -26,10 +31,11 @@ fun EventsManager(resources: Resources) {
                 when (event.type) {
                     TypeEvent.WA -> delay(event.duration!!.toLong() * 1000)
                     TypeEvent.SR -> break
+                    TypeEvent.RM -> eventsToDisplay.removeIf { it.eventName == event.eventName }
                     else -> eventsToDisplay.add(event)
                 }
+                if (event.type == TypeEvent.WA) eventsToDisplay.add(event)
             }
-
             eventList.removeAll(eventsToDisplay)
             eventList.removeAt(0)
             shouldRead.value = false
@@ -53,22 +59,39 @@ private fun DisplayEvents(
     onShouldReadChange: () -> Unit,
     onRemoveAllAction: () -> Unit,
 ) {
-    eventsToDisplay.forEach { event ->
-        when (event.type) {
-            TypeEvent.SP -> CustomButton(event, onClick = { onShouldReadChange.invoke() })
-            TypeEvent.TC -> Text(event.text!!)
-            TypeEvent.SO -> {}
-            TypeEvent.AI -> {}
-            TypeEvent.GM -> {}
-            TypeEvent.RM -> {}
-            TypeEvent.AT -> Text(event.text!!)
-            TypeEvent.MI -> {}
-            TypeEvent.STS -> {}
-            TypeEvent.CA -> {}
-            TypeEvent.DT -> {}
-            TypeEvent.TL -> {}
-            TypeEvent.RMA -> onRemoveAllAction.invoke()
-            else -> {}
+    val isOnClickContinueEnabled = remember { mutableStateOf(false) }
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .then(
+            if (isOnClickContinueEnabled.value) {
+                Modifier.clickable {
+                    onShouldReadChange.invoke()
+                    isOnClickContinueEnabled.value = false
+                }
+            } else {
+                Modifier
+            }
+        )) {
+        eventsToDisplay.forEach { event ->
+            when (event.type) {
+                TypeEvent.SP -> CustomButton(event, onClick = { onShouldReadChange.invoke() })
+                TypeEvent.TC -> CustomText(event)
+                TypeEvent.SO -> {}
+                TypeEvent.AI -> {}
+                TypeEvent.GM -> isOnClickContinueEnabled.value = true
+                TypeEvent.RM -> {}
+                TypeEvent.AT -> {
+                    CustomText(event, isAnimated = true)
+                }
+
+                TypeEvent.MI -> {}
+                TypeEvent.STS -> {}
+                TypeEvent.CA -> {}
+                TypeEvent.DT -> {}
+                TypeEvent.TL -> {}
+                TypeEvent.RMA -> onRemoveAllAction.invoke()
+                else -> {}
+            }
         }
     }
 }
---
+++ androidApp/src/main/java/com/ames/fr/android/ui/element/CustomText.kt
@@ -0,0 +1,50 @@
+package com.ames.fr.android.ui.element
+
+import android.util.Log
+import androidx.compose.foundation.layout.offset
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalConfiguration
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.ames.fr.data.model.Event
+import kotlinx.coroutines.delay
+
+@Composable
+fun CustomText(event: Event, isAnimated: Boolean = false) {
+    val fontSize = event.fontSize!!.sp
+    val originalText = event.text!!
+    val xPercentage = event.x!!
+    val yPercentage = event.y!!
+
+    val configuration = LocalConfiguration.current
+    val screenWidth = configuration.screenWidthDp
+    val screenHeight = configuration.screenHeightDp
+    val adjustedX = xPercentage * screenWidth
+    val adjustedY = yPercentage * screenHeight
+
+    val textToDisplay = remember { mutableStateOf(if (isAnimated) "" else originalText) }
+
+    LaunchedEffect(isAnimated) {
+        if (isAnimated) {
+            textToDisplay.value = ""
+            for (i in originalText.indices) {
+                delay((event.printSpeed!! * 1000).toLong())
+                textToDisplay.value += originalText[i]
+            }
+        }
+    }
+
+    Text(
+        text = textToDisplay.value,
+        fontSize = fontSize,
+        color = Color.White,
+        modifier = Modifier.offset(x = adjustedX.dp, y = adjustedY.dp)
+    )
+}
